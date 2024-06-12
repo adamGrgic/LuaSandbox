0d99fb1cc54956984e93590b849d5240205cfbb5
Adam Grgić <adam@grgicconsulting.com>
2024-06-11 22:10:27 -0600

    init

---
+++ main.lua
@@ -0,0 +1,14 @@
+
+message = "lua is awesome!"
+condition = 25
+
+
+if condition > 0 then
+    message = 1
+end
+
+
+function love.draw() 
+    love.graphics.setFont(love.graphics.newFont(50))
+    love.graphics.print(message);
+end
